59fb3bb9d116b71c63470d05257b6b83bcf7ec5f
antoniohcavero <antoniohcavero@gmail.com>
2025-04-14 17:45:28 +0000

    Creado carpeta "Pages"

---
+++ src/app/pages/bolsas/[bolsasId]/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function BolsaDetails({ params }){
+    return (
+        <h1>BOLSA: { params.bolsasId}</h1>
+    )
+
+}
---
+++ src/app/pages/bolsas/page.js
@@ -0,0 +1,14 @@
+//Es una ruta dinamica
+import React from "react";
+import Link from "next/link";
+
+export default function Bolsas() {
+
+    return (
+        <div>
+          <h1>Bolsas</h1>
+          <Link href="/bolsas/presupuesto">Bolsa Presupuesto</Link>
+          <Link href="/bolsas/inversion">Bolsa Inversion</Link>
+        </div>
+      );
+}
